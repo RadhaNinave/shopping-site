30c5960d1204d741a24faf6789804cb45385ce8c
RadhaNinave <radhikarao245@gmail.com>
2023-02-09 14:37:04 +0530

    login

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,82 +1,88 @@
-import React ,{useState , useRef , useContext}from 'react'
+import React, { useState, useRef, useContext } from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/esm/Container';
-const Login = () =>{
-    const emailInputRef=useRef();
-    const passwordInputRef=useRef();
-    const[isLogin , setIsLogin]=useState(true)
-    const[ isLoading, setIsLoding]=useState(false);
+const Login = () => {
+    const emailInputRef = useRef();
+    const passwordInputRef = useRef();
+    const [isLogin, setIsLogin] = useState(true)
+    const [isLoading, setIsLoding] = useState(false);
 
-    const switchAuthModeHandler=()=>{
-        setIsLogin((prev)=>!prev)
+    const switchAuthModeHandler = () => {
+        setIsLogin((prev) => !prev)
     }
-    const submitHandler = (event) =>{
+    const submitHandler = (event) => {
         event.preventDefault();
-        const enteredEmail=emailInputRef.current.value;
-        const enteredPassword=passwordInputRef.current.value;
+        const enteredEmail = emailInputRef.current.value;
+        const enteredPassword = passwordInputRef.current.value;
 
         setIsLoding(true);
-        if(isLogin)
-        {
-            
+        let url = "";
+        if (isLogin) {
+            url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAQx4rus_zRxC0qxoOFGFKmaULM5XQbkWY"
         }
-        else{
-            fetch("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAQx4rus_zRxC0qxoOFGFKmaULM5XQbkWY",
-            {               method :'POST',
-                body : JSON.stringify({
-                    email: enteredEmail,
-                    password : enteredPassword,
-                    returnSecureToken : true
-                }),
-                headers : {'Content-Typ0e' : 'application/json'}
-            }).then((res)=>{
-                setIsLoding(false);
-                if(res.ok)
-                {
-
-                }
-                else{
-                    res.json().then(data =>{
-                        let errorMessage="Authentication failed";
-                        if(data && data.error && data.error.message)
-                        {
-                            errorMessage=data.error.message
-                        }
-                        alert(errorMessage);
-                    })
-                }
-            });
+        else {
+            url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAQx4rus_zRxC0qxoOFGFKmaULM5XQbkWY";
         }
-
-    };
-    return(
-        <Container className='pt-5'>
-        <Form  className='pt-3' onSubmit={submitHandler}>
-            <h1 className='text-center' >{isLogin ? 'Log In' : "Sign Up"}</h1>
-          <Form.Group  className="mb-3">
-            <Form.Label htmlFor='email'>Email address</Form.Label>
-            <Form.Control id='email' type="email" placeholder="Enter email" required ref={emailInputRef}/>
-            
-          </Form.Group>
+        fetch(url , {
+            method:'POST',
+            body : JSON.stringify({
+                email: enteredEmail,
+                password : enteredPassword,
+                returnSecureToken : true
+            }),
+            headers : {'Content-Type' : 'application/json'},
+         }).then(res =>{
+            return res.json();
+         }).then(data=>{
+            if(isLogin){
+                console.log("Login Completed")
+                //console.log(data.idToken)
+                
+                alert("Log In Successful")
+            }else{
+                 console.log("Sign up Completed")
+                 alert("Sign Up Successful")
+                 
+            }
+         }).catch(err=>{
+            alert(err.message)
+           
+         })
     
-          <Form.Group className="mb-3">
-            <Form.Label htmlFor='password'>Password</Form.Label>
-            <Form.Control id='password' type="password" placeholder="Password" required ref={passwordInputRef}/>
-          </Form.Group>
-            
-     {!isLoading && <Button variant="primary" type="submit">
-        {isLogin ? 'Log In' : "Sign Up"}
-      </Button>} 
-      {isLoading && <p>Sending Request...</p>}
-      <div className='text-center pt-3'>
-      <button type='button' onClick={switchAuthModeHandler}>
-            {isLogin ? 'Create new account' : 'Login with existing account'}
-      </button>
-      </div>
-         
+    
+    };
+
+
+return (
+    <Container className='pt-5'>
+        <Form className='pt-3' onSubmit={submitHandler}>
+            <h1 className='text-center' >{isLogin ? 'Log In' : "Sign Up"}</h1>
+            <Form.Group className="mb-3">
+                <Form.Label htmlFor='email'>Email address</Form.Label>
+                <Form.Control id='email' type="email" placeholder="Enter email" required ref={emailInputRef} />
+
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+                <Form.Label htmlFor='password'>Password</Form.Label>
+                <Form.Control id='password' type="password" placeholder="Password" required ref={passwordInputRef} />
+            </Form.Group>
+
+            {!isLoading && <Button variant="primary" type="submit">
+                {isLogin ? 'Log In' : "Sign Up"}
+            </Button>}
+          
+            {!isLoading && <p></p>}
+            <div className='text-center pt-3'>
+                <button type='button' onClick={switchAuthModeHandler}>
+
+                    {isLogin ? 'Create new account' : 'Login with existing account'}
+                </button>
+            </div>
+
         </Form>
-        </Container>
-    )
+    </Container>
+)
 }
 export default Login;
